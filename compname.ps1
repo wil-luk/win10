69e9361499cd0a02ff7681d56d5a37775c844738
wil-luk <46342834+wil-luk@users.noreply.github.com>
2019-01-09 16:57:41 +0800

    Update compname.ps1

--- compname.ps1
+++ compname.ps1
@@ -1,5 +1,5 @@
 $password = Get-Content "$PSScriptroot\Password.txt" | ConvertTo-SecureString
-$domain = "hkbuad.local"
-$username = "$domain\chluk" 
+$domain = "your domain"
+$username = "$domain\your username" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
 Add-Computer -DomainName $domain -Credential $credential
