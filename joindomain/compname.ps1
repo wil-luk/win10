4cca58287f4fddaabd2447334ee00bd76496b56e
wil-luk <46342834+wil-luk@users.noreply.github.com>
2019-01-10 12:53:49 +0800

    Update compname.ps1

--- joindomain/compname.ps1
+++ joindomain/compname.ps1
@@ -1,8 +1,4 @@
-# Win 7 will has error
-#$password = Get-Content "$PSScriptroot\Password.txt" | ConvertTo-SecureString
-
-$password0 = Get-Content "$PSScriptroot\Password.txt" 
-$password = $password0 | ConvertTo-SecureString 
+$password = Get-Content "$PSScriptroot\Password.txt" | ConvertTo-SecureString
 $domain = "your domain"
 $username = "$domain\your username" 
 $credential = New-Object System.Management.Automation.PSCredential($username,$password)
